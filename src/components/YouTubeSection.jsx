3ffb2fe009a6987a52b30ad277cfbe01d97307cc
FarellAlva <farell6646@gmail.com>
2025-10-10 18:36:47 +0700

    fix version v2

--- src/components/YouTubeSection.jsx
+++ src/components/YouTubeSection.jsx
@@ -14,6 +14,7 @@ const YouTubeSection = () => {
 
     useEffect(() => {
         fetchYouTubeData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const extractChannelId = (channelUrl) => {
